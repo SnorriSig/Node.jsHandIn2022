ea23e0b589aa380a437af2cc6491f34c8c7aa12b
Snorri <snor0259@stud.kea.dk>
2022-02-19 16:15:17 +0100

    Clock exercise

---
+++ 3._ClockWebsite/app.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const express = require("express");
+const { log } = require('console');
+const app = express();
+
+const publicDirectoryPath = path.join(__dirname,"/public")
+app.use(express.static(publicDirectoryPath))
+
+// app.get("/", (req, res) =>{
+//     res.sendFile(__dirname + "/public/index.html")
+// });
+
+app.get("/new_york", (req, res) => {
+    res.sendFile(__dirname + "/public/new_york.html")
+});
+
+app.get("/reykjavik", (req, res) => {
+    res.sendFile(__dirname + "/public/reykjavik.html")
+});
+
+app.get("/copenhagen", (req, res) => {
+    res.sendFile(__dirname + "/public/copenhagen.html");
+});
+
+app.get("/moscow", (req, res) => {
+    res.sendFile(__dirname + "/public/moscow.html");
+});
+
+app.get("/tokyo", (req, res) => {
+    res.sendFile(__dirname + "/public/tokyo.html");
+});
+
+app.listen(8080, () => {
+    console.log("Server running on port", 8080);
+});
