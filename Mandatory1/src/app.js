7e4c78150747c2449f52c79541ff220627bfe0b4
Snorri <snor0259@stud.kea.dk>
2022-03-05 13:50:19 +0100

    ver1

--- Mandatory1/src/app.js
+++ Mandatory1/src/app.js
@@ -1,17 +1,62 @@
 const path = require("path");
 const express = require("express");
-const app = express()
+const app = express();
 const fs = require("fs");
 
 app.use(express.static("public"));
 
-const frontpage = fs.readFileSync("./public/pages/frontpage/frontpage.html").toString();
+const nav = fs.readFileSync("./public/components/nav.html").toString();
+
+const frontpage = fs.readFileSync("./public/pages/0._frontpage.html").toString();
+const commandLine = fs.readFileSync("./public/pages/1._commandLine.html").toString();
+const javaScript = fs.readFileSync("./public/pages/2._javascript.html").toString();
+const restApi = fs.readFileSync("./public/pages/3._restApi.html").toString();
+const nodeJs = fs.readFileSync("./public/pages/4._nodeJs.html").toString();
+const packageJson = fs.readFileSync("./public/pages/5._packageJson.html").toString();
+const expressModule = fs.readFileSync("./public/pages/6._express.html").toString();
+const nodemon = fs.readFileSync("./public/pages/7._nodemon.html").toString();
+
+const commandLinePage = nav.replace("%%DOCUMENT_TITLE%%", "Command-line") + commandLine;
+const javaScriptPage = nav.replace("%%DOCUMENT_TITLE%%", "JavaScript") + javaScript;
+const restApiPage = nav.replace("%%DOCUMENT_TITLE%%", "REST API") + restApi;
+const nodeJsPage = nav.replace("%%DOCUMENT_TITLE%%", "JavaScript") + nodeJs;
+const packageJsonPage = nav.replace("%%DOCUMENT_TITLE%%", "JavaScript") + packageJson;
+const expressModulePage = nav.replace("%%DOCUMENT_TITLE%%", "JavaScript") + expressModule;
+const nodemonPage = nav.replace("%%DOCUMENT_TITLE%%", "JavaScript") + nodemon;
 
 app.get("/", (req, res) => {
     res.send(frontpage);
 });
 
-const PORT = process.env.PORT || 8080
+app.get("/commandline", (req,res)=>{
+    res.send(commandLinePage);
+});
+
+app.get("/javascript", (req,res)=>{
+    res.send(javaScriptPage);
+});
+
+app.get("/restapi", (req,res)=>{
+    res.send(restApiPage);
+});
+
+app.get("/nodejs", (req,res)=>{
+    res.send(nodeJsPage);
+});
+
+app.get("/packagejson", (req,res)=>{
+    res.send(packageJsonPage);
+});
+
+app.get("/express", (req,res)=>{
+    res.send(expressModulePage);
+});
+
+app.get("/nodemon", (req,res)=>{
+    res.send(nodemonPage);
+});
+
+const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, () => {
     console.log("Server is running on port", server.address().port);
-})
+});
